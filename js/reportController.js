265e724bd08dffc5ef2e7487eacfc67015da4804
matthew brozen <matthewbrozen@gmail.com>
2016-11-07 16:59:29 -0800

    finalizing

--- js/reportController.js
+++ js/reportController.js
@@ -6,38 +6,15 @@ ReportController.$inject = ['$http'];
 // main function
 function ReportController($http) {
   var self = this;
-  self.all = {};
-  self.data=[];
   self.newReport={};
   self.addReport= addReport;
-
-//get all method needs to be turned into get last post or current user or session post
-function getReport(){
- $http
-   .get('https://frozen-gorge-77961.herokuapp.com/reports')
-   .then(function(response){
-     self.all = response.data.allReports;
- });
-}
-
-//calling the method at runtime to have data
-getReport();
+  self.currentDate = new Date();
 
 //post report method need to store _id from response
 function addReport(){
  $http
    .post('https://frozen-gorge-77961.herokuapp.com/reports', self.newReport)
    .success(function(response){
- });
-}
-
-
-//patch to report but _id is undefined, need to track it from post
-function updateReport(){
-  getReport();
- $http
-   .patch('https://frozen-gorge-77961.herokuapp.com/reports')
-   .then(function(response){
  });
 }
 
